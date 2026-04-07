c963f61aece9ce0bb00408c1b3d4e39c447fa827
Silas Lankford <silaslankford@gmail.com>
2026-04-07 18:16:45 -0400

    godot

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -13,5 +13,21 @@ return {
 
 		vim.cmd("colorscheme rose-pine-moon")
 		transparency.apply()
+
+    -- Godot specific highlighting fixes
+    local function apply_gdscript_ts_highlights()
+      vim.api.nvim_set_hl(0, "@function.call.gdscript", { link = "Function" })
+      vim.api.nvim_set_hl(0, "@function.method.call.gdscript", { link = "Function" })
+      vim.api.nvim_set_hl(0, "@constructor.gdscript", { link = "Type" })
+      vim.api.nvim_set_hl(0, "@property.gdscript", { link = "Identifier" })
+      vim.api.nvim_set_hl(0, "@variable.parameter.gdscript", { link = "Identifier" })
+      vim.api.nvim_set_hl(0, "@keyword.function.gdscript", { link = "Keyword" })
+    end
+
+    apply_gdscript_ts_highlights()
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      callback = apply_gdscript_ts_highlights,
+    })
 	end,
 }
