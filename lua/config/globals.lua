1b6439cc70a206860b8f790dd913f75f456304cf
Silas Lankford <silaslankford@gmail.com>
2026-05-18 13:43:37 -0400

    fix first open of day if_nil null telescope error

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -4,3 +4,7 @@
 -- ================================================================================================
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+if vim.nonnil == nil and vim.F and vim.F.if_nil then
+  vim.nonnil = vim.F.if_nil
+end
