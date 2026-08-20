6230a092eed2f09d488ff82aad2d332f3d998a4e
Silas Lankford <silaslankford@gmail.com>
2026-08-20 15:43:18 -0400

    disable update popup, change resize/rotate hotkeys

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -43,5 +43,5 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "rose-pine" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
